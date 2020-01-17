2722ca1720efe7a0eb614d8d517e9119f42e236e
ayushgupta2959 <37008291+ayushgupta2959@users.noreply.github.com>
2020-01-17 20:11:14 +0530

    updated scheduler

--- routes/schedule.js
+++ routes/schedule.js
@@ -1,23 +1,26 @@
 const express = require("express"),
   router = express.Router({ mergeParams: true }),
-  Task = require("../models/task");
+  Task = require("../models/task"),
+  { isLoggedIn } = require("../middlewares/index");
 
-router.route("/schedule").get((req, res) => {
-  if (req.user) {
-    Task.find({})
-      .where("assigned_to")
-      .in([req.user._id])
-      .sort([
-        ["due_date", "asc"],
-        ["priority", "asc"]
-      ])
-      .exec((err, tasks) => {
-        if (err) {
-          console.log(err);
-          res.redirect("/projects");
-        } else {
-          res.send({ tasks: tasks });
-        }
-      });
-  }
+router.use(isLoggedIn);
+
+router.get('/schedule',(req, res) => {
+  Task.find({})
+    .where("assigned_to")
+    .in([req.user._id])
+    .sort([
+      ["due_date", "asc"],
+      ["priority", "asc"]
+    ])
+    .exec((err, tasks) => {
+      if (err) {
+        console.log(err);
+        res.redirect("/");
+      } else {
+        res.send({ tasks: tasks });
+      }
+    });
 });
+
+module.exports = router;
